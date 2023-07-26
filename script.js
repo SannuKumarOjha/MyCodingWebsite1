81b5bb2db32f6f01f41c5bc80d550e31c9af7d9b
Sannu Kumar Ojha <137702131+SannuKumarOjha@users.noreply.github.com>
2023-07-26 18:58:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const openSidebar = () =>{
+   document.getElementById('firstSide').style.left="0" ;
+}
+
+const closeBtn = () =>{
+   document.getElementById('firstSide').style.left="-100%" ;
+}
+
+var preloader = document.getElementById('loading');
+
+const WebLoder = () =>{
+   document.getElementById('loading').style.display = 'none' ;
+}
